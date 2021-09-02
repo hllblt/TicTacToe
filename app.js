78075b4e25183205ab957a99e6931a6c5044e783
Halil <halill.bulutt.1@gmail.com>
2021-09-02 14:29:59 +0200

    gives "bingo" when three "o" or "x" completed

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 let gameSituation = "moving";
 let turn = "✖";
 let cell = "";
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
 
-const check = (id) => {
-  //   console.log("hi ", id);
+const check = () => {
+  if ((one.innerHTML == two.innerHTML) && (two.innerHTML == three.innerHTML) && one.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((one.innerHTML == four.innerHTML) && (four.innerHTML == seven.innerHTML) && one.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((one.innerHTML == five.innerHTML) && (five.innerHTML == nine.innerHTML) && one.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((two.innerHTML == five.innerHTML) && (five.innerHTML == eight.innerHTML) && two.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((three.innerHTML == five.innerHTML) && (five.innerHTML == seven.innerHTML) && three.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((three.innerHTML == six.innerHTML) && (six.innerHTML == nine.innerHTML) && three.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((four.innerHTML == five.innerHTML) && (five.innerHTML == six.innerHTML) && four.innerHTML != "") {
+    console.log("bingo");
+  }
+  else if ((seven.innerHTML == eight.innerHTML) && (eight.innerHTML == nine.innerHTML) && seven.innerHTML != "") {
+    console.log("bingo");
+  }
+
+};
+
+const asign = (id) => {
   cell = document.getElementById(id);
   if (gameSituation == "moving") {
     if (turn == "✖" && cell.innerHTML == "") {
       cell.innerHTML = "✖";
       turn = "◯";
-      console.log(id);
-      console.log(cell);
-      console.log(cell.innerHTML);
-      console.log(turn);
-    }
-    else if (turn == "◯" && cell.innerHTML == "") {
+    } else if (turn == "◯" && cell.innerHTML == "") {
       cell.innerHTML = "◯";
       turn = "✖";
-      console.log(id);
-      console.log(cell);
-      console.log(cell.innerHTML);
-      console.log(turn);
     }
   }
+  check();
 };
