7210b6306a0a89597f70f430ed6015bda8a2dca9
Halil <halill.bulutt.1@gmail.com>
2021-09-02 17:43:25 +0200

    completed adding counter to scoreboard

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let gameSituation = "moving";
 let turn = "✖";
 let cell = "";
+let scoreX = 0;
+let scoreO = 0;
 const one = document.getElementById("one");
 const two = document.getElementById("two");
 const three = document.getElementById("three");
@@ -10,33 +12,94 @@ const six = document.getElementById("six");
 const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
+let winner = "";
+
+const reset = (win) => {
+  alert("Winner of this tour is: "+ win);
+  // document.getElementById("scoreX").innerHTML = 0;
+  // document.getElementById("scoreO").innerHTML = 0;
+  let cells = document.getElementsByClassName("cells");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerHTML = "";
+  }
+};
+
+const score = (side) => {
+  if (side == "✖") {
+    scoreX += 1;
+    document.getElementById("scoreX").innerHTML = scoreX;
+  } else if (side == "◯") {
+    scoreO += 1;
+    document.getElementById("scoreO").innerHTML = scoreO;
+  }
+};
 
 const check = () => {
-  if ((one.innerHTML == two.innerHTML) && (two.innerHTML == three.innerHTML) && one.innerHTML != "") {
-    console.log("bingo");
+  if (
+    one.innerHTML == two.innerHTML &&
+    two.innerHTML == three.innerHTML &&
+    one.innerHTML != ""
+  ) {
+    winner = one.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    one.innerHTML == four.innerHTML &&
+    four.innerHTML == seven.innerHTML &&
+    one.innerHTML != ""
+  ) {
+    winner = one.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    one.innerHTML == five.innerHTML &&
+    five.innerHTML == nine.innerHTML &&
+    one.innerHTML != ""
+  ) {
+    winner = one.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    two.innerHTML == five.innerHTML &&
+    five.innerHTML == eight.innerHTML &&
+    two.innerHTML != ""
+  ) {
+    winner = two.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    three.innerHTML == five.innerHTML &&
+    five.innerHTML == seven.innerHTML &&
+    three.innerHTML != ""
+  ) {
+    winner = three.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    three.innerHTML == six.innerHTML &&
+    six.innerHTML == nine.innerHTML &&
+    three.innerHTML != ""
+  ) {
+    winner = three.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    four.innerHTML == five.innerHTML &&
+    five.innerHTML == six.innerHTML &&
+    four.innerHTML != ""
+  ) {
+    winner = four.innerHTML;
+    score(winner);
+    reset(winner);
+  } else if (
+    seven.innerHTML == eight.innerHTML &&
+    eight.innerHTML == nine.innerHTML &&
+    seven.innerHTML != ""
+  ) {
+    winner = seven.innerHTML;
+    score(winner);
+    reset(winner);
   }
-  else if ((one.innerHTML == four.innerHTML) && (four.innerHTML == seven.innerHTML) && one.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((one.innerHTML == five.innerHTML) && (five.innerHTML == nine.innerHTML) && one.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((two.innerHTML == five.innerHTML) && (five.innerHTML == eight.innerHTML) && two.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((three.innerHTML == five.innerHTML) && (five.innerHTML == seven.innerHTML) && three.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((three.innerHTML == six.innerHTML) && (six.innerHTML == nine.innerHTML) && three.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((four.innerHTML == five.innerHTML) && (five.innerHTML == six.innerHTML) && four.innerHTML != "") {
-    console.log("bingo");
-  }
-  else if ((seven.innerHTML == eight.innerHTML) && (eight.innerHTML == nine.innerHTML) && seven.innerHTML != "") {
-    console.log("bingo");
-  }
-
 };
 
 const assign = (id) => {
@@ -45,13 +108,13 @@ const assign = (id) => {
     if (turn == "✖" && cell.innerHTML == "") {
       cell.innerHTML = "✖";
       turn = "◯";
-      document.getElementById("turnO").innerHTML = "Your Turn"
-      document.getElementById("turnX").innerHTML = ""
+      document.getElementById("turnO").innerHTML = "Your Turn";
+      document.getElementById("turnX").innerHTML = "";
     } else if (turn == "◯" && cell.innerHTML == "") {
       cell.innerHTML = "◯";
       turn = "✖";
-      document.getElementById("turnX").innerHTML = "Your Turn"
-      document.getElementById("turnO").innerHTML = ""
+      document.getElementById("turnX").innerHTML = "Your Turn";
+      document.getElementById("turnO").innerHTML = "";
     }
   }
   check();
